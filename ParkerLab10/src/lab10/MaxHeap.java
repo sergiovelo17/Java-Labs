c3da383a910a6238f635b8f140137031be422a8e
sergiovelo17 <sergiovelo17@gmail.com>
2019-10-31 17:56:14 -0400

    final heap push

--- ParkerLab10/src/lab10/MaxHeap.java
+++ ParkerLab10/src/lab10/MaxHeap.java
@@ -78,9 +78,50 @@ public class MaxHeap {
     // This function implements the heap sort
     public void sort()
     {       
-    	// FILL IN
+    	 int n = this.heap.length; 
+    	  
+         // Build heap (rearrange array) 
+         for (int i = n / 2 - 1; i >= 0; i--) 
+             heapify(this.heap, n, i); 
+   
+         // One by one extract an element from heap 
+         for (int i=n-1; i>=0; i--) 
+         { 
+             // Move current root to end 
+             int temp = this.heap[0]; 
+             this.heap[0] = this.heap[i]; 
+             this.heap[i] = temp; 
+   
+             // call max heapify on the reduced heap 
+             heapify(this.heap, i, 0); 
+         } 
     }  
     
+    public void heapify(int arr[], int n, int i) 
+        { 
+            int largest = i; // Initialize largest as root 
+            int l = 2*i + 1; // left = 2*i + 1 
+            int r = 2*i + 2; // right = 2*i + 2 
+      
+            // If left child is larger than root 
+            if (l < n && arr[l] > arr[largest]) 
+                largest = l; 
+      
+            // If right child is larger than largest so far 
+            if (r < n && arr[r] > arr[largest]) 
+                largest = r; 
+      
+            // If largest is not root 
+            if (largest != i) 
+            { 
+                int swap = arr[i]; 
+                arr[i] = arr[largest]; 
+                arr[largest] = swap; 
+      
+                // Recursively heapify the affected sub-tree 
+                heapify(arr, n, largest); 
+            } 
+        } 
     
 	// DO NOT CHANGE THIS FUNCTION!
 	public void printArray(){
@@ -242,7 +283,7 @@ public class MaxHeap {
          
          if (!maxHeap6.IsEqual(arr17))
         	System.err.println("TEST FAILED: constructor 3");
-         /*
+         
          MaxHeap maxHeap7 = new MaxHeap(0);
          maxHeap7.heap = arr15;
          maxHeap7.index_to_last_element = 0;
@@ -266,7 +307,7 @@ public class MaxHeap {
         	System.err.println("TEST FAILED: sort 3");
         
         System.out.println("Testing Done (assignment) !!!");
-        */
+        
                 
     }
 }
